b4984b26571c8957151561a92f7c92749dde4ff4
Even-July <2363954051@qq.com>
2021-11-05 21:40:31 +0800

    完成用户登/注册功能

---
+++ src/DbOperator/sqlOperator.java
@@ -0,0 +1,84 @@
+package DbOperator;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class sqlOperator {
+
+    private Statement statement;
+
+    public sqlOperator(){
+        getDbConnection dbConnection=new getDbConnection();
+        this.statement=dbConnection.getConnection();
+        System.out.println("准备数据库操作完毕!");
+    }
+
+    // 以下是相关数据库的操作，包括增删改查
+    // 判断用户是否可以登录成功
+    public int isExist(String account, String password) throws SQLException {
+        String sql="select * from accountInfo where account='"+account+"' and PASSWORD='"+password+"'";
+        System.out.println("sql:"+sql);
+        ResultSet resultSet=statement.executeQuery(sql);
+        if(resultSet.next()){
+            return resultSet.getInt("userLevel");
+        }else{
+            sql="select * from accountInfo where mail='"+account+"' and PASSWORD='"+password+"'";
+            System.out.println("SQL:"+sql);
+            resultSet=statement.executeQuery(sql);
+            if(resultSet.next()){
+                return resultSet.getInt("userLevel");
+            }else{
+                return -1;
+            }
+        }
+    }
+
+    // 用户注册
+    public Boolean Register(String account, String password, String mail) throws SQLException {
+        System.out.println("注册!");
+        String date=new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+        System.out.println(date);
+        String sql="insert into applyInfo (account, PASSWORD, mail, applyDate) VALUES('"+account+"','"+password+"','"+mail+"','"+date+"')";
+        System.out.println("sql:"+sql);
+        if(statement.executeUpdate(sql)>0){
+            return true;
+        }else{
+            return false;
+        }
+
+    }
+
+    // 判断邮箱或用户名是否已注册或已申请注册
+    public int isExisted(String content,String key) throws SQLException {
+        String sql = null;
+        String sql1= null;
+        if(key.equals("mail")){
+            sql="select * from accountInfo where mail='"+content+"'";
+            sql1="select * from applyInfo where mail='"+content+"'";
+        }else if(key.equals("account")){
+            sql="select * from accountInfo where account='"+content+"'";
+            sql1="select * from applyInfo where account='"+content+"'";
+        }
+        System.out.println("SQL:"+sql);
+        ResultSet resultSet=statement.executeQuery(sql);
+        if(resultSet.next()){
+            // -1 说明用户账户或邮箱已经存在
+            return -1;
+        }else {
+            System.out.println("SQL:"+sql1);
+            resultSet=statement.executeQuery(sql1);
+            if(resultSet.next()){
+                // -2 说明用户账户或邮箱已经被申请注册，但尚未完成审核
+                return -2;
+            }else{
+                // 0表示用户账户或邮箱可以申请注册，合法
+                return 0;
+            }
+        }
+    }
+}
---
+++ src/Servlet/ServletAction.java
@@ -0,0 +1,157 @@
+package Servlet;
+
+import DbOperator.sendCode;
+import DbOperator.sqlOperator;
+import org.apache.commons.mail.EmailException;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class ServletAction extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        System.out.println("执行doGet!");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
+        System.out.println("执行doPost!");
+
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("执行service!");
+        req.setCharacterEncoding("UTF-8");
+        // 返回给前端的json
+        JSONObject jsonObject=new JSONObject();
+        String action=req.getParameter("Action");
+        sqlOperator sqlOp=new sqlOperator();
+        switch (action){
+            case "login":{
+                String account=req.getParameter("account");
+                String password=req.getParameter("password");
+                try {
+                    int level= sqlOp.isExist(account,password);
+                    if(level>=0){
+                        jsonObject.put("level",level);
+                        jsonObject.put("ok",200);
+                        jsonObject.put("account",account);
+                    }else{
+                        jsonObject.put("ok",404);
+                    }
+                    responseBack(req,resp,jsonObject);
+                } catch (SQLException | JSONException e) {
+                    System.out.println("数据库查询出现异常！");
+                    e.printStackTrace();
+                }
+                break;
+            }
+
+            case "register":{
+                String registerAccount=req.getParameter("account");
+                String password=req.getParameter("password");
+                String mail=req.getParameter("mail");
+                String code=req.getParameter("code");
+                try {
+                    if(sqlOp.Register(registerAccount,password,mail)){
+                        jsonObject.put("ok",200);
+                    }else{
+                        jsonObject.put("ok",400);
+                    }
+                    responseBack(req,resp,jsonObject);
+                } catch (SQLException | JSONException e) {
+                    e.printStackTrace();
+                }
+
+                break;
+            }
+
+            case "isExisted":{
+                String mail=req.getParameter("mail");
+                String account=req.getParameter("account");
+                System.out.println("mail:"+mail+",account:"+account);
+                String content,key;
+                if(mail == null){
+                    content=account;
+                    key="account";
+                }else{
+                    content=mail;
+                    key="mail";
+                }
+                System.out.println("content:"+content+",key:"+key);
+                try {
+                    int status=sqlOp.isExisted(content,key);
+                    if(status==-1){
+                        jsonObject.put("ok",500);
+                    }else if(status==-2) {
+                        jsonObject.put("ok",400);
+                    }else if(status==0){
+                        jsonObject.put("ok",200);
+                    }
+                    responseBack(req,resp,jsonObject);
+                } catch (SQLException | JSONException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+
+            case "sendCode":{
+                String mail=req.getParameter("mail");
+                sendCode sendC=new sendCode(mail);
+                try {
+                    if(sendC.Send()){
+                        jsonObject.put("ok",200);
+                        System.out.println("验证码:"+sendC.getCode());
+                        jsonObject.put("code",sendC.getCode());
+                    }else{
+                        jsonObject.put("ok",404);
+                    }
+                    responseBack(req,resp,jsonObject);
+                } catch (EmailException | JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            default:{
+                break;
+            }
+        }
+    }
+
+    private void responseBack(HttpServletRequest request, HttpServletResponse response, JSONObject json) throws JSONException {
+        boolean isAjax=true;if (request.getHeader("x-requested-with") == null || request.getHeader("x-requested-with").equals("com.tencent.mm")){isAjax=false;}	//判断是异步请求还是同步请求，腾讯的特殊
+        if(isAjax){
+            response.setContentType("application/json; charset=UTF-8");
+            try {
+                response.getWriter().print(json);
+                response.getWriter().flush();
+                response.getWriter().close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            System.out.println("异步方式");
+//            String action=json.getString("action");
+//            String errorNo="0";
+//            String errorMsg="ok";
+//            String url = result.jsp?action="+action+"&result_code="+errorNo+ "&result_msg=" + errorMsg;
+//            try {
+//                response.sendRedirect(url);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+            }
+        }
+
+
+}
