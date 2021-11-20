6a5af6800983c145085a8bcbc8496a9d2d7f834d
unknown <782237375@qq.com>
2021-11-20 21:06:35 +0800

    submit working_module

--- src/DbOperator/sqlOperator.java
+++ src/DbOperator/sqlOperator.java
@@ -235,4 +235,152 @@ public class sqlOperator {
         json.put("aaData",jsonList);
         return json;
     }
+
+    public JSONObject showSalaryTable(String isOrdered) throws SQLException, JSONException {
+        String sql;
+        System.out.println("sqlOperator中isOrdered="+isOrdered);
+        if(isOrdered.equals("true")){
+            System.out.println("按工资降序");
+            sql="select * from salary_file order by salary_number desc ";
+        }
+        else{
+            sql="select * from salary_file";
+        }
+
+        ArrayList jsonList=new ArrayList();
+        ArrayList jsonName=new ArrayList();
+        try {
+            ResultSet rs = statement.executeQuery(sql);
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int fieldCount = rsmd.getColumnCount();
+            while (rs.next()) {
+                Map map = new HashMap();
+                for (int i = 0; i < fieldCount; i++) {
+                    map.put(rsmd.getColumnName(i + 1), rs.getString(rsmd.getColumnName(i + 1)));
+                }
+                jsonList.add(map);
+            }
+            rs.close();
+            //加表头信息
+            for(int i=0;i<rsmd.getColumnCount();i++){
+                String columLabel= rsmd.getColumnLabel(i+1);
+                jsonName.add(columLabel);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("[queryRecord]查询数据库出现错误：" + sql);
+        }
+        JSONObject json=new JSONObject();
+        json.put("aaFieldName",jsonName);
+        json.put("aaData",jsonList);
+        return json;
+    }
+
+    public Boolean addSalaryRecord(String employee_id,String employee_name,String employee_duty,String salary_number,String salary_remark,String salary_month) throws SQLException, JSONException {
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        String sql="insert into salary_file(employee_id,employee_name,employee_duty,salary_number,salary_remark,salary_month,salary_datetime)";
+        sql=sql+" values('"+employee_id+"'";
+        sql=sql+" ,'"+employee_name+"'";
+        sql=sql+" ,'"+employee_duty+"'";
+        sql=sql+" ,'"+salary_number+"'";
+        sql=sql+" ,'"+salary_remark+"'";
+        sql=sql+" ,'"+salary_month+"'";
+        sql=sql+" ,'"+ dateTime.format(formatter) +"')";
+        try {
+            statement.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("[queryRecord]查询数据库出现错误：" + sql);
+            return false;
+        }
+        return true;
+    }
+
+    public Boolean deleteSalaryRecord(String salary_id) throws SQLException, JSONException {
+        String sql="delete from salary_file where salary_id="+salary_id;
+
+        try {
+            statement.executeUpdate(sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("[queryRecord]查询数据库出现错误：" + sql);
+            return false;
+        }
+        return true;
+    }
+
+    public Boolean modifySalaryRecord(String salary_id,String employee_id,String employee_name,String employee_duty,String salary_number,String salary_remark,String salary_month) throws SQLException, JSONException {
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        String sql="update salary_file set employee_id='"+employee_id+"',employee_name='"+employee_name+"',employee_duty='"+employee_duty+"',salary_number='"+salary_number+"',salary_remark='"+salary_remark+"',salary_month='"+salary_month+"',salary_datetime='"+dateTime.format(formatter)+"'";
+        sql+=" where salary_id="+salary_id+";";
+        try {
+            statement.executeUpdate(sql);
+            System.out.println("成功执行sql语句："+sql);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("[queryRecord]查询数据库出现错误：" + sql);
+            return false;
+        }
+        return true;
+    }
+
+    public JSONObject querySalaryRecord(String employee_name,String salary_month) throws SQLException, JSONException {
+        String sql="select * from salary_file where employee_name like '"+employee_name+"' or salary_month='"+salary_month+"';";
+        ArrayList jsonList=new ArrayList();
+        System.out.println("执行的sql语句为："+sql);
+
+        try {
+            ResultSet rs = statement.executeQuery(sql);
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int fieldCount = rsmd.getColumnCount();
+            while (rs.next()) {
+                Map map = new HashMap();
+                for (int i = 0; i < fieldCount; i++) {
+                    map.put(rsmd.getColumnName(i + 1), rs.getString(rsmd.getColumnName(i + 1)));
+                }
+                jsonList.add(map);
+            }
+            rs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("[queryRecord]查询数据库出现错误：" + sql);
+        }
+
+        JSONObject json=new JSONObject();
+        json.put("ok",200);
+        json.put("aaData",jsonList);
+        return json;
+    }
+
+    public JSONObject statisticSalaryTable() throws SQLException, JSONException {
+        String sql;
+        sql="select employee_name,salary_number as salary_num from salary_file group by employee_id;";
+
+        ArrayList jsonList=new ArrayList();
+        try {
+            ResultSet rs = statement.executeQuery(sql);
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int fieldCount = rsmd.getColumnCount();
+            while (rs.next()) {
+                Map map = new HashMap();
+                for (int i = 0; i < fieldCount; i++) {
+                    map.put(rsmd.getColumnName(i + 1), rs.getString(rsmd.getColumnName(i + 1)));
+                }
+                jsonList.add(map);
+            }
+            rs.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("[queryRecord]查询数据库出现错误：" + sql);
+        }
+        JSONObject json=new JSONObject();
+        json.put("aaData",jsonList);
+        json.put("ok",200);
+        return json;
+    }
+
 }
