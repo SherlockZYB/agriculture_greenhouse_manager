e84e793269b9eab506412fd32b8673f5254346ed
Sherlock_ZYB <1015462621@qq.com>
2021-12-03 11:33:52 +0800

    提交预警模块

---
+++ src/warningModule/dao/getDbConnection.java
@@ -0,0 +1,45 @@
+package warningModule.dao;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class getDbConnection {
+
+    // 数据库链接相关常量
+
+    final String http="jdbc:mysql://www.ylxteach.net:3366/";
+    final String dbName="xm08";
+
+
+    private Statement statement;
+
+    // 构造函数，
+    public getDbConnection(){
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+
+            String connStr=http+dbName+"?user=Administrator&password=XWClassroom20202023&useUnicode=true&characterEncoding=UTF-8";
+
+            Connection connection= DriverManager.getConnection(connStr);
+            statement=connection.createStatement();
+        }catch (Exception e){
+            System.out.println("数据库链接失败！");
+            e.printStackTrace();
+        }
+    }
+
+
+    // 返回数据库链接
+    public Statement getConnection(){
+        return this.statement;
+    }
+
+    // 关闭数据库
+    public void close() throws SQLException {
+        this.statement.close();
+        System.out.println("数据库关闭");
+    }
+}
